28d069e19640ec3ef66e28b2ff3aaf0bb4a94f46
Chanjung Kim <fman1335@gmail.com>
2017-06-12 13:42:27 +0900

    update

---
+++ index.php
@@ -0,0 +1,47 @@
+<html>
+    <link rel="stylesheet" href="style.css">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+    <title>Korean Lab</title>
+</head>
+<body>
+    <header><a href="index.php">Korean Lab</a></header>
+    <div class="search">
+        <input type="text" id="user_input" />
+        <input type="button" value="search" onclick="alert(document.getElementById('user_input').value)"/>
+    </div>
+    <nav>
+        <div>
+            <ul id="horiNav">
+              <li><a href="https://discord.gg/j9Knkyc">Chat</a></li>
+              <li><a href="beg.php">Beginner</a></li>
+              <li><a href="int.php">Intermediate</a></li>
+              <li><a href="adv.php">Advance</a></li>
+              <li><a href="dic.php">Dictionary</a></li>
+            </ul>
+        </div>
+    </nav>
+    <nav>
+        <div class="menu">
+            <ol>
+                <li><a href="#">Introduction</a></li>
+                <li><a href="index.php">Basic Expression</a></li>
+                <li><a href="#">Hangul</a></li>
+                <li><a href="#">Basic Grammar</a></li>
+            </ol>
+        </div>
+    </nav>
+    <article>
+        <div class="youtubeWrap">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/FuM6Y41PoV0" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <div>
+            <p>이번 강좌에서는...Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+    </article>
+</body>
+<footer>
+    <hr><p>Written and coded by </hr><a href="https://github.com/chanjungkim" target="_blank">Chanjung Kim</a>.</p>
+</footer>
+</html>
